99c9eecfc9c52019fc027194f63fdef8b16c4b75
Tejas B Achar <30596600+Tejas-Achar@users.noreply.github.com>
2021-05-30 19:30:13 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from bs4 import BeautifulSoup
+import requests
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/",methods = ["POST"])
+def scrapeData():
+    content = request.json
+    url = content["url"]
+    tagname = content["tagname"]
+    k = []
+    s = ""
+    if "https://" or  "http://" not in url:
+        url = "https://" + url
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+
+    if tagname != "a":
+        y = soup.find_all(tagname)
+        for i in y:
+            # print(i.text)
+            k.append(i.text)
+
+        for i in k:
+            s = s + i + "\n"
+
+        return s
+    else:
+        y = y = soup.find_all(tagname, href=True)
+        for i in y:
+            # print(i.text)
+            k.append(i['href'])
+
+        for i in k:
+            s = s + i + "\n"
+        return s
+
+
+# y = scrapeData("https://www.kleverme.com","p")
+# print(y)
+
+app.run()
