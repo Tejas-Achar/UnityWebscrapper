76cd8bb6a0f2ba3c4bff814b52e13cb9b0a02818
Tejas B Achar <30596600+Tejas-Achar@users.noreply.github.com>
2021-05-31 12:51:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from bs4 import BeautifulSoup
 import requests
 from flask import request
+import re
 
 app = Flask(__name__)
 
@@ -20,8 +21,18 @@ def scrapeData():
         url = "https://" + url
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
-            
-    if tagname != "a":
+    
+    if tagname == "email":
+        y = soup.find_all(string = re.compile('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'))
+        for i in y:
+            k.append(i)
+        for i in k:
+            s = s + i + "\n"
+        if s != "":
+            return s
+        else :
+            return tagname + " : Data not found"
+    if tagname != "a" or tagname != "email":
         y = soup.find_all(tagname)
         for i in y:
             # print(i.text)
